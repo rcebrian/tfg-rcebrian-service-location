4b25d4d6d235377d9afb2de74d22f7b72fc193a4
rcebrian <rodrigocebriang@gmail.com>
2021-04-30 00:02:27 +0200

    :sparkles: firebase location post

--- src/api/controllers/location.controller.ts
+++ src/api/controllers/location.controller.ts
@@ -1,7 +1,27 @@
 import httpStatus from 'http-status';
 import { Request, Response } from 'express';
 
+import * as jwt from 'jsonwebtoken';
+import { firebaseRepository, GeoPoint, Timestamp } from '../repository/firebase/firebase.repository';
+
 export const registerLocation = (req: Request, res: Response) => {
-  res.status(httpStatus.NOT_IMPLEMENTED)
-    .json({ message: 'NOT IMPLEMENTED' });
+  const bearer = req.headers.authorization;
+
+  // @ts-ignore: the validation is on middleware
+  const token = bearer.replace('Bearer ', '');
+
+  const { location, timestamp } = req.body;
+
+  const jwtSecret: any = process.env.JWT_SECRET;
+
+  const payload: any = jwt.verify(token, jwtSecret);
+  const userId = String(payload.id);
+
+  const docRef = firebaseRepository.collection('dev-devices').doc(userId).collection('locations').doc();
+  docRef.set({
+    location: new GeoPoint(location.latitude, location.longitude),
+    timestamp: Timestamp.fromDate(new Date(timestamp * 1000)),
+  }).then(() => {
+    res.status(httpStatus.CREATED).json();
+  });
 };
